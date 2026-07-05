c0eb7c3f667fb8f504e940b048d008a02bdde862
Bhargav Pedasingu <bhargavpedasingu@gmail.com>
2026-07-06 00:21:19 +0200

    Phase 6 JWT authentication

---
+++ backend/middleware/auth.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const authHeader = req.header('Authorization');
+
+  console.log("Authorization Header:", authHeader);
+
+  if (!authHeader) {
+    return res.status(401).json({
+      message: 'Access denied. No token provided.'
+    });
+  }
+
+  try {
+    const token = authHeader.replace('Bearer ', '');
+
+    console.log("Token:", token);
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    console.log("Decoded:", decoded);
+
+    req.user = decoded;
+
+    next();
+  } catch (err) {
+    console.error("JWT ERROR:", err);
+
+    return res.status(401).json({
+      message: err.message
+    });
+  }
+};
